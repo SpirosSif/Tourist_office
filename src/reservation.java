35df13b5089416b58d028095ba0cc822c1362248
spiro <spiro@LAPTOP-SBM34TKV>
2024-04-05 14:36:40 +0300

    done reservations to book a flight

--- src/reservation.java
+++ src/reservation.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -32,14 +33,13 @@ public class reservation extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jEditorPane1 = new javax.swing.JEditorPane();
-        to = new javax.swing.JComboBox<>();
-        from1 = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
+        search = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField1 = new javax.swing.JTextField();
+        frm = new javax.swing.JTextField();
+        to = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
+        from1 = new javax.swing.JComboBox<>();
         jMenuBar1 = new javax.swing.JMenuBar();
         home = new javax.swing.JMenu();
         login = new javax.swing.JMenu();
@@ -54,37 +54,29 @@ public class reservation extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        to.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        getContentPane().add(to, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 200, 380, 60));
-
-        from1.addActionListener(new java.awt.event.ActionListener() {
+        search.setBackground(new java.awt.Color(0, 204, 255));
+        search.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        search.setText("Αναζήτηση");
+        search.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                from1ActionPerformed(evt);
+                searchActionPerformed(evt);
             }
         });
-        getContentPane().add(from1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 200, 380, 60));
-
-        jButton1.setBackground(new java.awt.Color(0, 204, 255));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jButton1.setText("Αναζήτηση");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 293, 190, 40));
+        getContentPane().add(search, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 293, 190, 40));
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(60, 63, 65));
         jLabel3.setText("ΠΡΟΣ");
         getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 160, 70, -1));
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(60, 63, 65));
         jLabel2.setText("ΑΠΟ");
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 160, 60, -1));
-        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(63, 200, 380, 60));
+        getContentPane().add(frm, new org.netbeans.lib.awtextra.AbsoluteConstraints(63, 200, 380, 60));
 
-        jTextField1.setActionCommand("<Not Set>");
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 200, 380, 60));
+        to.setActionCommand("<Not Set>");
+        getContentPane().add(to, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 200, 380, 60));
 
         jLabel1.setIcon(new javax.swing.JLabel() {
             public javax.swing.Icon getIcon() {
@@ -99,6 +91,13 @@ public class reservation extends javax.swing.JFrame {
         }.getIcon());
         jLabel1.setText("jLabel1");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, 554));
+
+        from1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                from1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(from1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 200, 390, 60));
 
         home.setText("Αρχική");
         home.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -163,9 +162,76 @@ public class reservation extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void searchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+        if(frm.getText().trim().isEmpty() && to.getText().trim().isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "User Name and Password are empty");
+        }
+        else if(frm.getText().trim().isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "From form is empty");
+        }
+        else if(to.getText().trim().isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "To form is empty");
+        }
+        else
+        {
+            String from=frm.getText();
+            String todest=to.getText();
+            Connection myconnection;
+            String country1,country2,msg="";
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                String url ="jdbc:mysql://localhost/touristoffice";
+                String user1 = "root";
+                String password1 = "";
+                myconnection = DriverManager.getConnection(url,user1,password1);
+                if (myconnection != null)
+                System.out.println("Connected to the database touristoffice");
+                Statement stm=myconnection.createStatement();
+                ResultSet rs = stm.executeQuery("select * from country");
+                boolean found1=false,found2=false;
+                while(rs.next())
+                {
+                    country1=rs.getString(2);
+//                    country2=rs.getString(2);
+                
+                    if(from.equals(country1))
+                        found1=true;
+                    if(todest.equals(country1))
+                        found2=true;
+                    
+                    if(found2==true && found1==true){
+                        msg="There are flights";
+                        break;
+                    }
+                    else
+                    {
+                        msg= "There are no flights to that";
+                    }
+                        
+//                    if(from.equals(country1) || todest.equals(country1))
+//                    {
+//                        found=true;
+//                        
+//                        
+//                    }
+//                   
+//                    if(found==true)
+//                        break;
+                }
+            JOptionPane.showMessageDialog(null,msg);
+            myconnection.close();
+            }
+            catch(Exception e)
+            {
+                System.out.println("e");
+
+            }
+        }
+    }//GEN-LAST:event_searchActionPerformed
 
     private void registerMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_registerMouseClicked
         register r1=new register();
@@ -217,29 +283,29 @@ public class reservation extends javax.swing.JFrame {
     private void from1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_from1ActionPerformed
         // TODO add your handling code here:
         Connection myconnection;
-        
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                String url ="jdbc:mysql://localhost/touristoffice";
-                String user1 = "root";
-                String password1 = "";
-                myconnection = DriverManager.getConnection(url,user1,password1);
-                if (myconnection != null)
-                System.out.println("Connected to the database touristoffice");
-                
-                Statement stm=myconnection.createStatement();
-                ResultSet rs = stm.executeQuery("select * from country");
-                while(rs.next()){
-                    from1.addItem(rs.getString("c_name"));
-               
-                }
-                myconnection.close();
-    }
-            catch(Exception e)
-            {
-                System.out.println("e");
- 
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url ="jdbc:mysql://localhost/touristoffice";
+            String user1 = "root";
+            String password1 = "";
+            myconnection = DriverManager.getConnection(url,user1,password1);
+            if (myconnection != null)
+            System.out.println("Connected to the database touristoffice");
+
+            Statement stm=myconnection.createStatement();
+            ResultSet rs = stm.executeQuery("select c_name from country");
+            while(rs.next()){
+                from1.addItem(rs.getString("c_name"));
+
             }
+            myconnection.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("e");
+
+        }
     }//GEN-LAST:event_from1ActionPerformed
 
     /**
@@ -280,21 +346,20 @@ public class reservation extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu FAQ;
     private javax.swing.JMenu about_us;
+    private javax.swing.JTextField frm;
     private javax.swing.JComboBox<String> from1;
     private javax.swing.JMenu home;
-    private javax.swing.JButton jButton1;
     private javax.swing.JEditorPane jEditorPane1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     private javax.swing.JMenu login;
     private javax.swing.JMenu register;
     private javax.swing.JMenu reserv;
+    private javax.swing.JButton search;
     private javax.swing.JMenu sugg;
-    private javax.swing.JComboBox<String> to;
+    private javax.swing.JTextField to;
     // End of variables declaration//GEN-END:variables
 }
