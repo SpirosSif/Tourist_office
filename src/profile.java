7f1403161e862c770b158a055a8d8241b35bf8f4
Μαρία Παπαδοπούλου <papadopoyloy.mary@hotmail.com>
2024-04-18 18:44:35 +0300

    button log out in profile page

--- src/profile.java
+++ src/profile.java
@@ -1,6 +1,9 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -10,8 +13,34 @@ import javax.swing.JOptionPane;
  *
  * @author maria
  */
-public class profile extends javax.swing.JFrame {
+public class profile extends javax.swing.JFrame 
+{
+    public String username;
+    public profile(String username) {
+        
+ Connection myconnection;
+        this.username= username;
+        initComponents();
+        //String username1 = getUsernameFromDatabase(username);
+        usernamel.setText("Welcome " + username + "!");
+         try {
+         Class.forName("com.mysql.jdbc.Driver");
+                String url ="jdbc:mysql://localhost/touristoffice";
+                String user1 = "root";
+                String password1 = "";
+                myconnection = DriverManager.getConnection(url,user1,password1);
+                if (myconnection != null)
+                System.out.println("Connected to the database touristoffice");
+                Statement stm=myconnection.createStatement();
+                ResultSet rs = stm.executeQuery("select username from customers");
+               // return username;
+        } catch(Exception e)
+            {
+                System.out.println("e");
+            }
+    }           
 
+    
     /**
      * Creates new form profile
      */
@@ -33,7 +62,8 @@ public class profile extends javax.swing.JFrame {
         jTextArea1 = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
+        usernamel = new javax.swing.JLabel();
+        log_out = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         home = new javax.swing.JMenu();
         login = new javax.swing.JMenu();
@@ -85,28 +115,37 @@ public class profile extends javax.swing.JFrame {
 
         jPanel2.setBackground(new java.awt.Color(255, 204, 255));
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
-        jLabel2.setText("Καλωσόρισες ");
+        usernamel.setForeground(new java.awt.Color(102, 0, 0));
+        usernamel.setText("jLabel3");
+
+        log_out.setText("Log out");
+        log_out.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                log_outActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 792, Short.MAX_VALUE)
-            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel2Layout.createSequentialGroup()
-                    .addGap(273, 273, 273)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(274, Short.MAX_VALUE)))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(291, 291, 291)
+                .addComponent(usernamel, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(317, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(log_out, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(118, 118, 118))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 465, Short.MAX_VALUE)
-            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel2Layout.createSequentialGroup()
-                    .addGap(38, 38, 38)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(374, Short.MAX_VALUE)))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(85, 85, 85)
+                .addComponent(usernamel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 240, Short.MAX_VALUE)
+                .addComponent(log_out, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(70, 70, 70))
         );
 
         home.setText("Αρχική");
@@ -244,39 +283,25 @@ public class profile extends javax.swing.JFrame {
 
     }//GEN-LAST:event_FAQMouseClicked
 
+    private void log_outActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_log_outActionPerformed
+        // TODO add your handling code here:
+        JFrame frame=new JFrame("EXIT");
+       if(JOptionPane.showConfirmDialog(frame,"Υou want to logout "+username+"?", "EXIT", JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION)
+      {
+          System.exit(0);
+       }
+    }//GEN-LAST:event_log_outActionPerformed
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(profile.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(profile.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(profile.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(profile.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+ public static void main(String args[]) {
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            String username = "Maria"; // Αντικαταστήστε με το όνομα του συνδεδεμένου χρήστη
+            new profile(username).setVisible(true);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new profile().setVisible(true);
-            }
-        });
+    });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -284,16 +309,17 @@ public class profile extends javax.swing.JFrame {
     private javax.swing.JMenu about_us;
     private javax.swing.JMenu home;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JButton log_out;
     private javax.swing.JMenu login;
     private javax.swing.JMenu profile;
     private javax.swing.JMenu register;
     private javax.swing.JMenu reservation;
     private javax.swing.JMenu tips;
+    private javax.swing.JLabel usernamel;
     // End of variables declaration//GEN-END:variables
 }
